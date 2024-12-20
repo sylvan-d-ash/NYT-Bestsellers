536f6cff739ef82473a6464d3b9dff09e09d75ec
Sylvan Ash <sylvan.d.ash@gmail.com>
2024-12-20 14:00:22 +0300

    Add view model for books list view

---
+++ NYT Bestsellers/BooksViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  BooksViewModel.swift
+//  NYT Bestsellers
+//
+//  Created by Sylvan Ash on 20/12/2024.
+//
+
+import Foundation
+
+final class BooksViewModel {
+    private let category: Category
+
+    init(category: Category) {
+        self.category = category
+    }
+}
