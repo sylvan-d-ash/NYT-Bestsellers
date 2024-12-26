7505d8d9c4cdebb781b1b96ad999671f2017a927
Sylvan Ash <sylvan.d.ash@gmail.com>
2024-12-26 23:00:27 +0300

    Flesh out the view model a bit

--- NYT Bestsellers/BooksListViewController.swift
+++ NYT Bestsellers/BooksListViewController.swift
@@ -9,6 +9,16 @@ import UIKit
 
 final class BooksListViewController: UIViewController {
     private let tableview = UITableView(frame: .zero, style: .plain)
+    private let viewModel: BooksViewModel
+
+    init(category: Category) {
+        self.viewModel = BooksViewModel(category: category)
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
